88551ced58bb53b76ab87af8c0f482833da7f52a
hassayax <hassayax@HASSAYAX-MOBL1>
2016-07-02 17:08:42 +0300

    Helloperson-2.1, MulBoard-2.5

---
+++ HelloPerson/HelloPerson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What’s your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello "+ userName);
+            Console.WriteLine("Enter a number in range 1-10:");
+            string input = Console.ReadLine();
+            int n = 0;
+            string sp = "";
+            if (int.TryParse(input, out n) && n>0 && n<11)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(sp + userName);
+                    sp += " ";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not integer number or not in range.");
+            }
+        }
+    }
+}
---
+++ MulBoard/MulBoard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 10;
+            int col = 10;
+            
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= col; j++)
+                {
+
+                    Console.Write(String.Format("{0,4}", i*j ));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
