a63dcb28da301c36a16444fd1ffd45004c45b08d
hassayax <hassayax@HASSAYAX-MOBL1>
2016-07-04 12:19:33 +0300

    DollarStairs - 2.7

---
+++ DollarStairs/DollarStairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollarStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an integer number:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("$");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
