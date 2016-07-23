c07aed162d665328817a529fa1b635e8548a5da8
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-23 22:52:32 +0300

    2.4

--- Quad/Quad/Calc.cs
+++ Quad/Quad/Calc.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace Quad
 {
+    //Good dicision to extract this logic to a seperate class in a seperate file.
+    //Though, it would have been better to add also an interface :)
     public class Calc
     {
+        //The convension in C# is to declare each variable in its own line.
         double x1,x2;
 
         public string calcQuard(double a, double b, double c)
         {
             if (a == 0)
             {
+                //It's not a good idea to return strings.
                 //Console.WriteLine("a cannot be zero!");
                 return "a cannot be zero!";
             }
@@ -23,6 +27,8 @@ namespace Quad
             {
                 x1 = (-b + System.Math.Sqrt(sqrtpart)) / (2 * a);
                 x2 = (-b - System.Math.Sqrt(sqrtpart)) / (2 * a);
+
+                //Not a good idea to return such string as the result
                 return "x1="+x1+" x2="+x2;
             }
             else if (sqrtpart < 0)
