971fec2ac62f58268d0b9b2e9a765a4084a94944
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-23 22:54:24 +0300

    2.5

--- MulBoard/MulBoard/Program.cs
+++ MulBoard/MulBoard/Program.cs
@@ -17,7 +17,9 @@ namespace MulBoard
             {
                 for (int j = 1; j <= col; j++)
                 {
-
+                    //Console.Write has an overload with format. It was enough to write:
+                    //Console.Write("{0, 4", i*j);
+                    //Also, consider using the new C# 6 feature - string interpolation. where you can write Console.Write($"{i*j, 4}")
                     Console.Write(String.Format("{0,4}", i*j ));
                 }
 
